7805a7d111813be677fe27c7d9cd97a68f340b15
CodeXRook <lacekitfly@gmail.com>
2019-03-23 18:58:57 -0400

    create new firebase project

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+imprt app from 'firebase/app';
+import 'firebase/auth';
+
+var config = {
+    apiKey: "AIzaSyDXrCSXEkrCBQM8Boxyye3ndeDVHcVIgBc",
+    authDomain: "jampack-fron-end.firebaseapp.com",
+    databaseURL: "https://jampack-fron-end.firebaseio.com",
+    projectId: "jampack-fron-end",
+    storageBucket: "",
+    messagingSenderId: "760212620139"
+  };
+
+  app.initializeApp(config);
+
+  export default app;
