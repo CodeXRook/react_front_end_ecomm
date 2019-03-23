282acd25b8cff11322a5f07af03ae9773bda1684
CodeXRook <lacekitfly@gmail.com>
2019-03-23 19:45:56 -0400

    set up container

--- src/components/error404.js
+++ src/components/error404.js
@@ -4,6 +4,7 @@ export default (props) => {
     const { location } = props;
     return(
         <div className="jumbotron jumbotron-fluid">
+         <div className="container"></div>
         </div>
     );
 }
