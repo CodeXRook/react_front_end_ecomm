6768500d5269d36a8dc18b86847569c98be460a2
CodeXRook <lacekitfly@gmail.com>
2019-03-23 17:05:12 -0400

    added home in comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import React, { Component } from 'react';
+
 
 class App extends Component {
 
+
+
+
+    render(){
+        return(
+            <h1> HELLO AGAIN</h1>
+        );
+    }
 }
 
 export default App;
---
+++ src/containers/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../contexts/auth';
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+        
+        )
+    }
+}
