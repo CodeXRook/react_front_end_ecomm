dd9965243dd1c7f29cb9547e5cb69f1d69d88171
CodeXRook <lacekitfly@gmail.com>
2019-03-23 17:27:38 -0400

    adding create profile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
 
 
 
-
     render(){
         return(
-            <h1> HELLO AGAIN</h1>
+            <HashRouter>
+                <AuthContext.Provider value ={this.state.user}>
+                    <Route path='/' component={ Header }/>
+                    <div className='container mt-5'>
+                        <Switch>
+                            <Route path='/' excat component ={ Home } />
+                            <Route path='/createprofile' exact component { CreateProfile } />
+                        </Switch>
+                    </div>
+                </AuthContext.Provider>
+            </HashRouter>
+            
         );
     }
 }
