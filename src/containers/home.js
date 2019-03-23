abba761b2fa9e45a9e854a32c3faa921919e5a1d
CodeXRook <lacekitfly@gmail.com>
2019-03-23 17:08:48 -0400

    added user and email

--- src/containers/home.js
+++ src/containers/home.js
@@ -7,7 +7,16 @@ export default class Home extends React.Component {
     render() {
         return (
         <AuthContext.Consumer>
-            
+            {
+                (user) => {
+                    if (user) {
+                        return (
+                            <>
+                            <h2> Welcome back, {user.email} Let's shop </h2>
+                        )
+                    }
+                }
+            }
         </AuthContext.Consumer>
         )
     }
