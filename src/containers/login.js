d93c7300da31ca3658a579a9a47c89bb94e51955
CodeXRook <lacekitfly@gmail.com>
2019-03-23 18:29:11 -0400

    upated state in login

--- src/containers/login.js
+++ src/containers/login.js
@@ -2,3 +2,7 @@ import React from 'react';
 import firebase from '../firebase';
 import AuthContext from '../contexts/auth';
 import { Redirect } from 'react-router-dom';
+
+export default class Login extends React.Component {
+    
+}
