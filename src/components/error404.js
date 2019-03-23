e92e76f215dfc3517b10fcbd1c92c2ab349a1cd3
CodeXRook <lacekitfly@gmail.com>
2019-03-23 19:45:29 -0400

    set up jumbo

--- src/components/error404.js
+++ src/components/error404.js
@@ -2,4 +2,8 @@ import React from 'react';
 
 export default (props) => {
     const { location } = props;
+    return(
+        <div className="jumbotron jumbotron-fluid">
+        </div>
+    );
 }
