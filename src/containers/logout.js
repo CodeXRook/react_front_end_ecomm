7a2cbf275965ee0df796e8e98026c5d92d70baab
CodeXRook <lacekitfly@gmail.com>
2019-03-23 19:20:21 -0400

    added header

--- src/containers/logout.js
+++ src/containers/logout.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import firebase from '../firebase';
 
-export default class Logout extends React.Coomponent {
+export default class Logout extends React.Component {
 
     componentDidMount() {
         firebase.auth().signOut()
     }
 
     render() {
-        
+        return <h1>You are now logged out</h1>
     }
 }
