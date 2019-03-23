055ec5637006f7a4446692da342cea3addc3c6ec
CodeXRook <lacekitfly@gmail.com>
2019-03-23 19:48:13 -0400

    error completed

--- src/components/error404.js
+++ src/components/error404.js
@@ -6,6 +6,7 @@ export default (props) => {
         <div className="jumbotron jumbotron-fluid">
          <div className="container">
          <h1 className="display-4">Error 404</h1>
+         <p className="lead">Page{location.pathname} not found</p>
          </div>
         </div>
     );
