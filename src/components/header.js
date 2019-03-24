67d550d2c8e4e443c33d6c99d7a2fdfeaadb0250
CodeXRook <lacekitfly@gmail.com>
2019-03-23 20:25:35 -0400

    updated header

--- src/components/header.js
+++ src/components/header.js
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../contexts/auth';
 
 export default (props) => {
-    const loggedOut = <> <li className="nav-item">
-        <Link className="nav-link" to="/createprofile">Sign Up Now </Link>
+  const loggedOut = <><li className="nav-item">
+      <Link className="nav-link" to="/signup">Sign Up</Link>
     </li>
-        <li claasName="nav-item">
-            <Link className="nav-link" to="/login">Sign In</Link>
-        </li></>
-
-    const loggedIn = <> <li className="nav-item">
-        <Link className="nav-link" to="/logout"> Sign Out </Link>
+    <li className="nav-item">
+      <Link className="nav-link" to="/login">Login</Link>
     </li></>
 
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="/">JamPack</Link>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/"> Home </Link>
-                </li>
-                <AuthContext.Consumer>
-                    {
-                        user => {
-                            if (user) return loggedIn
-                            else return loggedOut
-                        }
-                    }
-                </AuthContext.Consumer>
-            </ul>
-        </nav>
-    )
+  const loggedIn = <><li className="nav-item">
+      <Link className="nav-link" to="/logout">Logout</Link>
+    </li></>
+
+  return(
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">Authentication</Link>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <AuthContext.Consumer>
+          {
+            user => {
+              if (user) return loggedIn
+              else return loggedOut
+            }
+          }
+        </AuthContext.Consumer>
+      </ul>
+    </nav>
+  )
 }
