02ba95b47f863dd3f6026c5507e0b60db514e0aa
CodeXRook <lacekitfly@gmail.com>
2019-03-23 20:12:19 -0400

    bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -6,30 +6,30 @@ export default (props) => {
     const loggedOut = <> <li className="nav-item">
         <Link className="nav-link" to="/createprofile">Sign Up Now </Link>
     </li>
-    <li claasName="nav-item">
-    <Link className="nav-link" to= "/login">Sign In</Link>   
+        <li claasName="nav-item">
+            <Link className="nav-link" to="/login">Sign In</Link>
+        </li></>
+
+    const loggedIn = <> <li className="nav-item">
+        <Link className="nav-link" to="/logout"> Sign Out </Link>
     </li></>
 
-    const loggedIn =<> <li className="nav-item">
-    <Link className ="nav-link" to="/logout"> Sign Out </Link>
-    </li></>
-
-    return(
+    return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">JamPack</Link>
-        <ul className="navbar-nav">
-        <li className="nav-item">
-        <Link className="nav-link" to="/"> Home </Link>
-        </li>
-        <AuthContext.Consumer>
-            {
-                user =>{
-                    if (user) return loggedIn
-                    else return loggedOut
-                }
-            }
-        </AuthContext.Consumer>
-        </ul>
+            <Link className="navbar-brand" to="/">JamPack</Link>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/"> Home </Link>
+                </li>
+                <AuthContext.Consumer>
+                    {
+                        user => {
+                            if (user) return loggedIn
+                            else return loggedOut
+                        }
+                    }
+                </AuthContext.Consumer>
+            </ul>
         </nav>
     )
 }
