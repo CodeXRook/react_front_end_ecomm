5acf48ba35c1fa10736ba35bc78f36a418150fc7
CodeXRook <lacekitfly@gmail.com>
2019-03-26 22:06:16 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Login from './containers/login';
 import Logout from './containers/logout';
 import Error404 from './components/error404';
 import Cart from '/.components/cart';
-import shopprofile from './components/Shopprofile';
+import Shopprofile from '/.components/shopprofile';
 
 // ---- CONTEXTS
 import AuthContext from './contexts/auth';
