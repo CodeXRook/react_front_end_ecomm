94c2334d8caf1a77ada936142a0747a35def05e0
CodeXRook <lacekitfly@gmail.com>
2019-03-26 22:56:55 -0400

    updated cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-export default (props) => {
-    return(
-    <h1>yo son</h1>
-    )
+class Cart extends Component {
+
+     
+    
 }
+    export default Cart;
