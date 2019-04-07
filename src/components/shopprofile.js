09bc10337a25cbbe5743d4e35cd9375e82d4f30e
CodeXRook <lacekitfly@gmail.com>
2019-04-07 15:25:05 -0400

    updated shopsprofile

--- src/components/shopprofile.js
+++ src/components/shopprofile.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-export default (props) => {
-    
-    return(
-    <h1>yo son</h1>
-    )
+
+class ShopProfile extends React.Component {
+    constructor(props){
+        super(props);
+                
+          this.state = {
+                shops: [{
+                shop_name: 'Game Boss',
+                games:[{
+                title:'spiderman',
+                release_date:2018,
+                console:'PS4'
+                }]
+              },
+
+              {}]
+        
+        }
+    }
+
 }
+
+export default ShopProfile;
