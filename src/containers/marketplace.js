ad05ff94d870583825ad5a569377cd03d025689a
CodeXRook <lacekitfly@gmail.com>
2019-04-06 20:44:46 -0400

    updated main page

--- src/containers/marketplace.js
+++ src/containers/marketplace.js
@@ -1,134 +1,140 @@
 import React from 'react';
-import  './marketplace.css';
+import './marketplace.css';
 
-class MarketPlace extends React.Component{
-    constructor(props){
-        super (props);
+class MarketPlace extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state ={
-            input:'',
+    this.state = {
+      input: '',
 
-            products: [{
-                id: 2,
-                name:'',
-                shop_id: 1,
-                price: 100,
-                description: 'jeans'
-            }],
-            cart: []
-        }
+      products: [{
+        id: 2,
+        name: '',
+        shop_id: 1,
+        price: 100,
+        description: 'jeans'
+      }],
+      cart: []
     }
+  }
 
-handleCartClick = (e) => {
+  handleCartClick = (e) => {
     const newCart = this.state.cart.concat(this.state.products[e.target.id])
     console.log('this is newCart', newCart);
-    localStorage.setItem('cart',JSON.stringify(newCart));
-    this.setState( { cart:newCart}, () =>{
-        console.log(this.state)
+    localStorage.setItem('cart', JSON.stringify(newCart));
+    this.setState({ cart: newCart }, () => {
+      console.log(this.state)
     })
-}
+  }
 
-    render(){
-        return (
-<>
-            <h1> Welcome To MarketPlace </h1>
+  render() {
+    return (
+     <>
 
-<div>
-  <p> Game Marketplace </p>
-<div class="gallery">
+        <h1> Welcome To MarketPlace </h1>
 
-<a target="_blank" href="img_5terre.jpg">
+     <div>
+          <p> Game Marketplace </p>
+          <div className="gallery">
+            <a target="_blank" href="img_5terre.jpg">
 
-  <img src={ require('../assets/2k19.jpeg')} alt="Cinque Terre" width="50" height="50"/>
-  
-</a>
+              <img src={require('../assets/2k19.jpeg')} alt="Cinque Terre" width="50" height="50" />
 
-<div class="desc"> NBA 2K19 </div>
+            </a>
+            <div className="desc"> NBA 2K19 </div>
 
-</div>
+          </div>
 
+          <div className="gallery">
+            <a target="_blank" href="img_forest.jpg">
+              <img src={require('../assets/StateOfDecay.jpeg')} alt="Forest" width="60" height="40" />
+            </a>
+            <div className="desc"> State Of Decay </div>
+          </div>
 
-<div className="gallery">
-<a target="_blank" href="img_forest.jpg">
-  <img src={ require('../assets/StateOfDecay.jpeg')} alt="Forest" width="60" height="40"/>
-</a>
-<div className="desc"> State Of Decay </div>
-</div>
+          <div className="gallery">
+            <a target="_blank" href="img_lights.jpg">
+              <img src={require('../assets/spiderMan.jpeg')} alt="Northern Lights" width="50" height="40" />
+            </a>
+            <div className="desc">Spider Man</div>
+          </div>
 
-<div className="gallery">
-<a target="_blank" href="img_lights.jpg">
-  <img src={ require('../assets/spiderMan.jpeg')} alt="Northern Lights" width="50" height="40"/>
-</a>
-<div className="desc">Spider Man</div>
-</div>
+          <div className="gallery">
+            <a target="_blank" href="img_mountains.jpg">
+              <img src={require('../assets/Ghost.jpeg')} alt="Mountains" width="60" height="40" />
+            </a>
+            <div className="desc"> GHOST </div>
+          </div>
 
-<div class="gallery">
-<a target="_blank" href="img_mountains.jpg">
-  <img src={require('../assets/Ghost.jpeg')} alt="Mountains" width="60" height="40"/>
-</a>
-<div className="desc"> GHOST </div>
-</div>
+          <div className="gallery">
+            <a target="_blank" href="img_forest.jpg">
+              <img src={require('../assets/fallOut4.jpeg')} alt="Forest" width="60" height="40" />
+            </a>
+            <div className="desc"> State Of Decay </div>
+        </div>
+        </div>
+      <section>
 
-<div className="gallery">
-<a target="_blank" href="img_forest.jpg">
-  <img src={ require('../assets/fallOut4.jpeg')} alt="Forest" width="60" height="40"/>
-</a>
-<div className="desc"> State Of Decay </div>
-</div>
-</div>
+      <p>FLASH SALES</p>
 
-
-<div>
-<p> Best Sellers </p>
-<div class="gallery">
-
-<a target="_blank" href="img_5terre.jpg">
-
-  <img src={ require('../assets/4ktv.webp')} alt="Cinque Terre" width="50" height="50"/>
-  
-</a>
-<div class="desc"> 4k HDTV </div>
-
-</div>
+<div className="w3-content">
+  <img className="mySlides" src="img_nature_wide.jpg" />
+  <img className="mySlides" src="img_snow_wide.jpg" />
+  <img className="mySlides" src={require('../assets/fallOut4.jpeg')} />
+  </div>
+      </section>
 
 
-<div className="gallery">
+          <div>
+            <p> Best Sellers </p>
+            <div className="gallery">
 
-<a target="dropdown" href="img_forest.jpg">
+              <a target="_blank" href="img_5terre.jpg">
 
-  <img src={ require('../assets/VR.jpeg')}  alt="Forest" width="60" height="40"/>
-</a>
-<div className="desc"> VR headset </div>
+                <img src={require('../assets/4ktv.webp')} alt="Cinque Terre" width="50" height="50" />
 
-</div>
+              </a>
+              <div className="desc"> 4k HDTV </div>
 
-<div className="gallery">
-<a target="_blank" href="img_lights.jpg">
-  <img src={ require('../assets/nike.jpeg')} alt="Northern Lights" width="50" height="40"/>
-</a>
-<div className="desc">Nike Vapormax</div>
-</div>
-
-<div class="gallery">
-<a target="_blank" href="img_mountains.jpg">
-  <img src={require('../assets/alexa.jpg')} alt="Mountains" width="60" height="40"/>
-</a>
-<div className="desc"> Hi Alexa </div>
+            </div>
 
 
+            <div className="gallery">
 
-</div>
+              <a target="dropdown" href="img_forest.jpg">
 
-<div className="gallery">
-<a target="_blank" href="img_forest.jpg">
-  <img src={ require('../assets/SummerTent.jpg')} alt="Forest" width="80" height="50"/>
-</a>
-<div className="desc"> Shop Tents </div>
-</div>
-</div>
+                <img src={require('../assets/VR.jpeg')} alt="Forest" width="60" height="40" />
+              </a>
+              <div className="desc"> VR headset </div>
+
+            </div>
+
+            <div className="gallery">
+              <a target="_blank" href="img_lights.jpg">
+                <img src={require('../assets/nike.jpeg')} alt="Northern Lights" width="50" height="40" />
+              </a>
+              <div className="desc">Nike Vapormax</div>
+            </div>
 
 
-      </>    
+            <div className="gallery">
+              <a target="_blank" href="img_mountains.jpg">
+                <img src={require('../assets/alexa.jpg')} alt="Mountains" width="60" height="40" />
+              </a>
+              <div className="desc"> Hi Alexa </div>
+            </div>
+
+
+            <div className="gallery">
+              <a target="_blank" href="img_forest.jpg">
+                <img src={require('../assets/SummerTent.jpg')} alt="Forest" width="80" height="50" />
+              </a>
+              <div className="desc"> Shop Tents </div>
+            </div>
+         </div>
+
+</>
 
         );
     }
